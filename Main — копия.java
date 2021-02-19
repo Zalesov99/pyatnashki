09175bac3588449460ea614f6254c1c3d2366482
Илья <47445574+Zalesov99@users.noreply.github.com>
2021-02-19 21:57:07 +0300

    Определение клиентской ширины экрана

--- Main — копия.java
+++ Main — копия.java
@@ -20,5 +20,37 @@ public class Main extends JFrame { // класс Main в данной работ
     private static Random generator = new Random(); // генератор случайных чисел
     private int[][] numbers = new int[4][4];
 
+/* -=== Опредиление клиентской ширины экрана ===- */
+
+
+
+    public Main() {
+        setTitle("Пятнашки"); //Заголовок окна
+
+
+
+        setSize (300, 300); // Задаем размеры окна приложения
+        setLocationRelativeTo(null); // Окно приложения центрируется относительно экрана
+
+        setResizable(true); // задаем возможность растягивать окно
+        createMenu(); //инициализируем меню
+        setJMenuBar(menu); // добавляем панель меню к окну
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // закрываем программу при закрытии окна
+
+        //Класс container — прямой подкласс класса component, и наследует все его методы.
+        //Каждый компонент перед выводом на экран помещается в контейнер (container). Контейнер "знает", как разместить компоненты на экране.
+        /*Создав компонент — объект класса Component или его расширения, следует добавить его к предварительно созданному объекту класса container
+        или его расширения одним из методов add (). */
+
+        Container container = getContentPane();
+        init();
+        panel.setDoubleBuffered(true);
+        panel.setBackground(Color.white); // устанавливаем цвет фона
+        container.add(panel); // добавление компонентов в контейнер
+        repaintField();
+        JLabel();
+
+    }
+
    
 }
