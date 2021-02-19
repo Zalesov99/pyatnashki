b27c00b991e07cb279b378228645c7055d14602c
Илья <47445574+Zalesov99@users.noreply.github.com>
2021-02-19 21:59:58 +0300

    Описание метода init

--- Main — копия.java
+++ Main — копия.java
@@ -51,6 +51,48 @@ public class Main extends JFrame { // класс Main в данной работ
         JLabel();
 
     }
+    public void init() { // описание метода init
+        int[] invariants = new int[16]; // инициализируем массив с именем invariants из 16 элементов - лт 0 до 15
+
+        for (int i = 0; i < 4; i++) { // перебираем элементы i от 0 до 3
+            for (int j = 0; j < 4; j++) { // перебираем элементы j от 0 до 3
+                numbers[i][j] = 0; // указываем что перебор в цикле начинается с нулевого элемента
+                invariants[i*4 + j] = 0; // определяем какой из 16 элементов будет = 0
+            }
+        }
+
+        for (int i = 1; i < 16; i++) { // перебираем елементы i от 1 до 15
+            int k; //обьявляем переменную k типа int
+            int l; //обьявляем переменную l типа int
+            do { // цикл с послеусловием
+                k = generator.nextInt(100) % 4; // переменной k присваиваем произвольное число от 0 до 100 деленное по модулю на 4
+                l = generator.nextInt(100) % 4; // переменной l присваиваем произвольное число от 0 до 100 деленное по модулю на 4
+            }
+            while (numbers[k][l] != 0); // до тех пор пока двумерный массив numbers не равен 0
+            numbers[k][l] = i; // присваиваем двумерному массиву numbers значение i в цикле от 1 до 15
+            invariants[k*4+l] = i; // определяем позиции всех елементов кроме 0 на сетке
+        }
+
+        boolean change = true; // в булевую переменную change заносим значение true
+        int counter = 1; // инициализируем переменную counter типа int и присваиваем ей 1
+        while (change) {
+            change = false;
+            for (int i = 0; i < 16; i++) {
+                if (invariants[i] != i) {
+                    for (int j = 0; j < 16; j++) {
+                        if (invariants[j] == i) {
+                            int temp = invariants[i];
+                            invariants[i] = invariants[j];
+                            invariants[j] = temp;
+                            change = true;
+                            counter++;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
 
    
 }
